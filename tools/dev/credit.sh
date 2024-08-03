b65b7f70495d09a92ac4a6648a326c2a2a52f3c7
Eric Larson <larson.eric.d@gmail.com>
2024-08-02 21:46:35 -0400

    FIX: No cloud

--- tools/dev/credit.sh
+++ tools/dev/credit.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
 set -eo pipefail
+THIS_DIR=$(dirname "$0")
+FILE_STATS_FILE="$THIS_DIR/file_stats.txt"
+CO_AUTHORS_FILE="$THIS_DIR/co_author.txt"
+# One idea is to use the local git log:
 # https://git-scm.com/docs/pretty-formats
 # first line has email then hash
-git log --no-merges --numstat --pretty="%aE	%aN	%H	x" | awk -F "	" '{ if (NF == 4){ email = $1; name = $2; hash = $3 }; if(NF == 3) {file=$3; print hash"	"file"	"name"	"email"	"$1"	"$2}}' > file_stats.txt
-git log --no-merges --format="%H %n%(trailers:key=Co-authored-by)" | awk '{ if (NF == 1){ commit = $1; }; if(NF > 1) { email=$(NF); print commit"	"email }}' > co_author.txt
+git log --no-merges --numstat --pretty="%aE	%aN	%H	x" | awk -F "	" '{ if (NF == 4){ email = $1; name = $2; hash = $3 }; if(NF == 3) {file=$3; print hash"	"file"	"name"	"email"	"$1"	"$2}}' > "$FILE_STATS_FILE"
+git log --no-merges --format="%H %n%(trailers:key=Co-authored-by)" | awk '{ if (NF == 1){ commit = $1; }; if(NF > 1) { email=$(NF); print commit"	"email }}' > "$CO_AUTHORS_FILE"
+
+# But this becomes very tricky with squash+merge vs old merge style. So instead,
+# let's query GH directly for the PR numbers and merge commits, then use
+# "git show" on those.
+#ARGS="-R mne-tools/mne-python"
+#PR_NUMBERS_FILE="$THIS_DIR/pr_numbers.json"
+#if [ ! -f "$PR_NUMBERS_FILE" ]; then
+#    echo "Listing PRs (this can take a while) ..."
+#    gh pr list $ARGS --limit 10000 --state merged --json number,title,mergeCommit > "$PR_NUMBERS_FILE"
+#fi
+#rm -f $FILE_STATS_FILE
+#for COMMIT in $(cat "$PR_NUMBERS_FILE" | jq -r '.[].mergeCommit.[]'); do
+#    echo "$COMMIT"
+#    # This will be way too slow:
+#    # gh pr diff $ARGS $PR_NUM
+#    # So instead use local information:
+#    git show $COMMIT --numstat --pretty="%aE	%aN	%H	x" | awk -F "	" '{ if (NF == 4){ email = $1; name = $2; hash = $3 }; if(NF == 3) {file=$3; print hash"	"file"	"name"	"email"	"$1"	"$2}}' >> $FILE_STATS_FILE
+#    git show $COMMIT --format="%H %n%(trailers:key=Co-authored-by)" | awk '{ if (NF == 1){ commit = $1; }; if(NF > 1) { email=$(NF); print commit"	"email }}' >> $CO_AUTHORS_FILE
+#done
--- tools/dev/update_changelog.py
+++ tools/dev/update_changelog.py
@@ -1,6 +1,6 @@
 """Compute code credit.
 
-Run ./tools/dev_reports/credit.sh first.
+Run ./tools/dev/credit.sh first.
 
 TODO:
 - Remove orphan by linking somewhere
@@ -9,45 +9,46 @@ TODO:
 from pathlib import Path
 from collections import defaultdict
 import glob
-import os
 import pathlib
 import pprint
-from matplotlib.colors import Normalize, ListedColormap
-from matplotlib.pyplot import get_cmap
+from textwrap import indent
+
 import mne
 import re
 
 import numpy as np
 import mne
-import wordcloud
 
-WORDCLOUD_FONT_PATH = os.getenv(
-    "WORDCLOUD_FONT_PATH",
-    "~/Library/Fonts/ClearviewHwy2W.ttf",  # on larsoner's macOS system
-)
-WORDCLOUD_FONT_PATH = str(Path(WORDCLOUD_FONT_PATH).expanduser().resolve(strict=True))
+repo_root = Path(__file__).parents[2]
+doc_root = repo_root / "doc"
+this_dir = Path(__file__).parent
+
+ignore_removals = False
 
 # make stats dict with filename keys
 stats = defaultdict(lambda: defaultdict(lambda: np.zeros(2, int)))
 ignores = {
     line.split("#")[0].strip(): list()
-    for line in Path(".git-blame-ignore-revs").read_text().strip().splitlines()
+    for line in (repo_root / ".git-blame-ignore-revs").read_text().strip().splitlines()
     if not line.startswith("#")
 }
 # Get parent commit for black (use with --first-parent)
 # ignores["16fe5b5dbb1622ddfb7269fab889bab44ef43212"] = ignores.pop(
 #    "e81ec528a42ac687f3d961ed5cf8e25f236925b0"
 # )
-# ignores.pop("e81ec528a42ac687f3d961ed5cf8e25f236925b0")  # black doesn't show up
 # MAINT: splitting utils.py to submodules (#5862)
 ignores["203a96cbba2732d2e349a8f96065e74bbfd2a53b"] = list()
 # API : refactor huge viz.py to have a proper module (still need to update tests)
 ignores["5c1d64b1045f96c7c3a7e45bc4068e19fce695e7"] = list()
+# MRG, DOC: Integrate manual parts #6767
+ignores["ff349f356edb04e1b5f0db13deda8d1a20aca351"] = list()
+# DOC : adding first version of converted mne manual WIP
+ignores["5e744ee2cc38629f7708a35a27358eec2525c95d"] = list()
 
 # Get "co-authored by" commits and start translating emails to actual names
 mailmap = dict()
 usernames = dict()
-for line in Path(".mailmap").read_text("utf-8").splitlines():
+for line in (repo_root / ".mailmap").read_text("utf-8").splitlines():
     name = re.match("^([^<]+) <([^<>]+)>", line.strip()).group(1)
     assert name is not None
     emails = list(re.findall("<([^<>]+)>", line.strip()))
@@ -65,13 +66,13 @@ for line in Path(".mailmap").read_text("utf-8").splitlines():
         else:
             mailmap[old] = new
 co_authorship = defaultdict(lambda: list())
-for line in Path("co_author.txt").read_text("utf-8").splitlines():
+for line in (this_dir / "co_author.txt").read_text("utf-8").splitlines():
     commit, email = line.strip().split("\t", maxsplit=1)
     email = email[1:-1]  # remove <>
     co_authorship[commit].append(mailmap.get(email, email))
 
 commits = defaultdict(lambda: 0)
-file_stats = Path("file_stats.txt").read_text("utf-8")
+file_stats = (this_dir / "file_stats.txt").read_text("utf-8")
 for line in file_stats.splitlines():
     commit, file, name, email, p, m = line.strip().split("\t")
     if email not in usernames:
@@ -120,21 +121,28 @@ for line in file_stats.splitlines():
     if p == "-" or m == "-":  # binary
         raise RuntimeError(f"Unexcluded binary file: {file}")
     p, m = int(p), int(m)
-    m = 0  # Ignore removals, they aren't all that helpful for us
+    if ignore_removals:
+        m = 0
     for name in names:
         commits[(name, commit)] += p + m
         stats[file][name] += [p, m]
 assert len(co_authorship) == 0
+unknown_emails = set(
+    email for email in unknown_emails
+    if "autofix-ci[bot]" not in email
+)
 assert len(unknown_emails) == 0, "\n".join(sorted(unknown_emails))
-# Report the biggest commits
+
+print("Biggest included commits:")
 commits = dict(
     (k, commits[k]) for k in sorted(commits, key=lambda k_: commits[k_], reverse=True)
 )
 for ni, name in enumerate(commits, 1):
     if ni > 10:
         break
-    print(f"{ni}. {name[1]} @ {commits[name]:5d} by {name[0]}")
+    print(f"{name[1]} @ {commits[name]:5d} by {name[0]}")
 
+print("\nIgnored commits:")
 # Report the ignores
 for commit in ignores:  # should have found one of each
     print(f"ignored {len(ignores[commit]):3d} files for hash {commit}")
@@ -230,6 +238,7 @@ for fname, counts in stats.items():
         if mod == "mne._fiff":
             raise RuntimeError
         mod_stats[mod][e] += pm
+        mod_stats["mne"][e] += pm
         total_lines += pm
 mod_stats.pop("null")  # stuff we shouldn't give credit for
 mod_stats = dict(
@@ -247,104 +256,105 @@ other_files = sorted(other_files)
 if len(other_files):
     pprint.pprint(other_files)
     raise RuntimeError(f"{len(other_files)} misc file(s) found")
-print(f"Total lines: {list(total_lines)}")
+print(f"\nTotal line change count: {list(total_lines)}")
 
-max_font_size = 40
-min_font_size = 6
-# cmap = mne.viz.mne_analyze_colormap(limits=[0, 1, 2])
-# cmap = (cmap[:, :3] * cmap[:, [-1]] / 255. + 0.5 * (255 - cmap[:, [-1]]))[:128][::-1]
-# cmap = ListedColormap(cmap / 255.)
-cmap = get_cmap("plasma")
-norm = Normalize(-min_font_size, max_font_size * 1.2, clip=True)
-
-
-def color_func(word, font_size, position, orientation, font_path, random_state):
-    out = tuple((np.array(cmap(norm(font_size))[:3]) * 255).round().astype(int))
-    assert len(out) == 3
-    return "rgb({:.0f}, {:.0f}, {:.0f})".format(*out)
-
-
-font_path = str(Path(WORDCLOUD_FONT_PATH).expanduser().resolve())
-with open("doc/credit.rst", "w", encoding="utf-8") as fid:
+BDG_KINDS = ["bdg-primary-line", "bdg-info-line"]
+with open(doc_root / "credit.rst", "w", encoding="utf-8") as fid:
     fid.write(
-        """
+        f"""
 :orphan:
-:html_theme.sidebar_secondary.remove:
+
+.. _contributors:
+
+============
+Contributors
+============
+
+There are many different ways to contribute to MNE-Python! So far we only list
+code contributions below, but plan to add other metrics in the future.
 
 .. _code_credit:
 
 Code credit
 ===========
 
+Below are lists of code contributors to MNE-Python. The numbers in parentheses are the
+number of lines changed in our code history.
+
+- :{BDG_KINDS[0]}:`This Color` is used for the top 10% of contributors.
+- :{BDG_KINDS[1]}:`This Color` is used for the remaining 90% of contributors.
+
 .. raw:: html
 
    <style>
-     p.sd-card-text {
-       font-size: 0.9rem;
-     }
-     .bd-main .bd-content .bd-article-container {
-         max-width: 90em;
-     }
-     img.transparent-background {
-         background: unset !important;
-     }
-     div.sd-card-body {
-       max-height: 300px;
-     }
+   /* Make it occupy more page width */
+   .bd-main .bd-content .bd-article-container {{
+       max-width: 90vw;
+   }}
+   /* Limit max card height */
+   div.sd-card-body {{
+     max-height: 15em;
+   }}
    </style>
 
-.. grid:: 1 1 2 2
-   :gutter: 2
+Entire codebase
+---------------
 
-"""
-    )
+""")
     for mi, (mod, counts) in enumerate(mod_stats.items()):
+        if mi == 0:
+            assert mod == "mne", mod
+        elif mi == 1:
+            fid.write("""
+
+By submodule
+------------
+
+Contributors often have domain-specific expertise, so we've broken down the
+contributions by submodule as well below.
+
+.. grid:: 1 2 3 3
+   :gutter: 1
+
+""")
         # if there are 10 this is 100, if there are 100 this is 100
-        height = int(round(100 * np.log10(len(counts))))
-        cloud = wordcloud.WordCloud(
-            width=800,
-            height=height,
-            max_font_size=max_font_size,
-            min_font_size=min_font_size,
-            font_path=font_path,
-            prefer_horizontal=1,
-            random_state=mi,
-            color_func=color_func,
-            mode="RGBA",
-            background_color="rgb(0,0,0)",
-        )
         these_stats = dict((k, v.sum()) for k, v in counts.items())
         these_stats = dict(
             (k, these_stats[k])
             for k in sorted(these_stats, key=lambda x: these_stats[x], reverse=True)
         )
-        svg_path = pathlib.PurePosixPath(
-            f"wordcloud/{mod.replace('.', '_').replace(' ','_').replace('(','').replace(')','')}.svg"
-        )
-        cloud.generate_from_frequencies(these_stats)
-        svg_path_file = "doc" / Path(svg_path)
-        svg_path_file.parent.mkdir(exist_ok=True)
-        svg_code = cloud.to_svg(embed_font=True)
-        rect_fill = "fill:rgb(0,0,0)"
-        assert svg_code.count(rect_fill) == 1
-        svg_code = svg_code.replace(rect_fill, f"{rect_fill};fill-opacity:0.1")
-        svg_path_file.write_text(svg_code, encoding="utf-8")
         kind = "blame" if mod in mod_file_map else "tree"
         link_mod = mod_file_map.get(mod, mod.replace(".", "/"))
         link = f"https://github.com/mne-tools/mne-python/{kind}/main/{link_mod}"
-        fid.write(
+        # Use badges because they flow nicely, inside a grid to make it more compact
+        stat_lines = []
+        for ki, (k, v) in enumerate(these_stats.items()):
+            idx = 0 if ki < (len(these_stats) - 1) // 10 + 1 else 1
+            if "[bot]" in k:
+                continue
+            stat_lines.append(
+                f":{BDG_KINDS[idx]}:`{k} ({format(v, ",")})`"
+            )
+        stat_lines = " ".join(stat_lines)
+        if mi == 0:
+            fid.write(
+            f"""
+
+.. card:: {mod}
+   :class-card: overflow-auto
+   :link: https://github.com/mne-tools/mne-python/graphs/contributors
+
+   {stat_lines}
+
+""")
+        else:
+            fid.write(
             f"""
 
    .. grid-item-card:: {mod}
       :class-card: overflow-auto
       :link: {link}
 
-      .. image:: {svg_path}
-         :alt: {mod} word cloud
-         :width: 100%
-         :class: transparent-background
+      {stat_lines}
 
-      {', '.join(these_stats)}
-
-"""
-        )
+""")
